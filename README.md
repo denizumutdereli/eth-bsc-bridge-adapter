d55bcdd544dafa2c3f8489a0500dd88cbf523b4f
Deniz Umut Dereli <197686+denizumutdereli@users.noreply.github.com>
2022-05-16 13:28:15 +0300

    eth-bsc and bsc-eth burning, minting cross bridge adapter init.

--- README.md
+++ README.md
@@ -1,2 +1,65 @@
 # eth-bsc-bridge-adapter
 ETH to BSC or BSC to ETH Cross Bridge Adapter, including Burning and Minting Template
+
+PS. Use it with filled ETH/Rinkeby and BSC testnet faucets for testing.
+
+https://faucet.rinkeby.io/
+
+https://testnet.binance.org/faucet-smart
+
+
+> npm install
+
+
+Bridge contract on ETH
+
+> truffle migrate --reset --network ethTestnet
+
+
+Bridge contract on BSC
+
+
+> truffle migrate --reset --network bscTestnet
+
+
+Ok we have both smart contracts bridges on ETH and BSC
+Now lets check the token balances on ETH and BSC networks.
+
+ETH Network balance (which should be 1000 ref:scripts/eth-bsc-transfer.js line 6)
+
+> truffle exec scripts/eth-token-balance.js --network ethTestnet 
+
+BSC Network balance (which should be 0)
+
+> truffle exec scripts/bsc-token-balance.js --network bscTestnet
+
+
+Open a new terminal to build the bridge between networks, its going to listen transactions (currently no .env its hardcode. Be sure the you are on the right network!)
+
+Listening from bridge contract on the ETH 
+
+> node scripts/eth-bsc-bridge.js
+
+Create transaction from another terminal:
+
+> truffle exec scripts/eth-bsc-transfer.js --networkTestnet
+
+You should see the bridge api is going to detect the transaction event on previous terminal
+Which means its burned on ETH and minted on BSC and the tokens are now available.
+
+
+Summarize and checking the current balances
+
+ETH Network Balance should be 0
+
+> truffle exec scripts/eth-token-balance.js --network ethTestnet
+
+BSC Network Balance should be 1000
+
+> truffle exec scripts/bsc-token-balance.js --network bscTestnet
+
+
+All done,
+Cheers.
+
+@denizumutdereli
---
+++ truffle-config.js
@@ -0,0 +1,83 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const mnemonic = '';
+
+module.exports = {
+  /**
+   * Networks define how you connect to your ethereum client and let you set the
+   * defaults web3 uses to send transactions. If you don't specify one truffle
+   * will spin up a development blockchain for you on port 9545 when you
+   * run `develop` or `test`. You can ask a truffle command to use a specific
+   * network from the command line, e.g
+   *
+   * $ truffle test --network <network-name>
+   */
+
+  networks: {
+    // Useful for testing. The `development` name is special - truffle uses it by default
+    // if it's defined here and no other network is specified at the command line.
+    // You should run a client (like ganache-cli, geth or parity) in a separate terminal
+    // tab if you use this network and you must also set the `host`, `port` and `network_id`
+    // options below to some value.
+    //
+    // development: {
+    //  host: "127.0.0.1",     // Localhost (default: none)
+    //  port: 8545,            // Standard Ethereum port (default: none)
+    //  network_id: "*",       // Any network (default: none)
+    // },
+    // Another network with more advanced options...
+    // advanced: {
+    // port: 8777,             // Custom port
+    // network_id: 1342,       // Custom network
+    // gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
+    // gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
+    // from: <address>,        // Account to send txs from (default: accounts[0])
+    // websockets: true        // Enable EventEmitter interface for web3 (default: false)
+    // },
+    // Useful for deploying to a public network.
+    // NB: It's important to wrap the provider as a function.
+
+    
+    ethTestnet: {
+      provider: () => new HDWalletProvider(
+        "86b6d6c4fb5be69c8c6757e5a5b157ef35964dbc7459abb26ae16fb5ebbfe114", //or mnemonic 
+        'https://rinkeby.infura.io/v3/baab6a1c72174240b4d74e208fb2481f',
+      ),
+      network_id: 4, //rinkeby
+      skipDryRun: true
+    },
+    bscTestnet: {
+      provider: () => new HDWalletProvider(
+        "86b6d6c4fb5be69c8c6757e5a5b157ef35964dbc7459abb26ae16fb5ebbfe114", //or mnemonic 
+        'https://data-seed-prebsc-1-s1.binance.org:8545'
+      ),
+      network_id: 97,
+      skipDryRun: true
+    },
+    // Useful for private networks
+    // private: {
+    // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
+    // network_id: 2111,   // This network is yours, in the cloud.
+    // production: true    // Treats this network as if it was a public net. (default: false)
+    // }
+  },
+
+  // Set default mocha options here, use special reporters etc.
+  mocha: {
+    // timeout: 100000
+  },
+
+  // Configure your compilers
+  compilers: {
+    solc: {
+      version: "0.8.13",    // Fetch exact version from solc-bin (default: truffle's version)
+      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
+      // settings: {          // See the solidity docs for advice about optimization and evmVersion
+      //  optimizer: {
+      //    enabled: false,
+      //    runs: 200
+      //  },
+      //  evmVersion: "byzantium"
+      // }
+    }
+  }
+};
